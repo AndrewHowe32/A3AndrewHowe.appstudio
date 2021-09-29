2b836cef6722ccf2a5a79b7717bff9fd9fdd42f5
AndrewHowe32 <andrewhowe@creighton.edu>
2021-09-29 17:39:01 -0500

    dogStore

---
+++ forms/functionCompare/functionCompare.js
@@ -0,0 +1,11 @@
+/*
+let pass1 = prompt("Enter a password: ")
+let pass2 = prompt("Enter a alternative password: ")
+
+function compare(pass1, pass2) {
+   let equal = pass1.localeCompare(pass2)
+   return (equal == 0) ? "The passwords are the same": "The passwords are not the same"
+}
+
+console.log(compare(pass1, pass2))
+*/
