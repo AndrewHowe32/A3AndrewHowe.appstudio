d6bfe22a54808891cd7dd174d99b52a9837ca2dd
AndrewHowe32 <andrewhowe@creighton.edu>
2021-09-29 17:15:45 -0500

    functionCompare

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,22 @@
+/*
+let cities = []
+let endCities = false
+let moreCities = ""
+let i = 0
+
+while (endCities == false) {
+  moreCities = prompt("Do you have more cities? Yes or No?")
+  if (moreCities == "Yes") {
+  endCities = false
+  additionalCity = prompt ("Please Enter a new city")
+  cities.push(additionalCity)
+} else {
+    endCities = true
+}
+}
+
+while (i < cities.length) {
+    console.log(cities[i].toLowerCase())
+    i += 1
+}
+*/
